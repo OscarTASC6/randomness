2bfea9eb47b3de0ea700aedd39133b7e57cd23f6
OscarTASC6 <oscart5292@rfwagner.org>
2019-07-11 14:19:52 -0400

    numbers_

---
+++ randomness.js
@@ -0,0 +1,42 @@
+const alphabet = "abcedfghijklmnopqrstuvwxyz"
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+function randomNumber(){
+    const number = Math.random()*21;
+    const final = Math.floor(number);
+console.log(final)
+};
+randomNumber();
+function randomLetter(){
+    
+
+
+};
+// randomLetter();
+// function randomFood(foods){
+
+// };
+// randomFood();
+// function randomGroup(){
+
+// };
+// function randomGroup(){
+
+
+// }
+
